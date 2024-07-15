41fb0c3908a16bc470136219aaa38a991e6ab36c
jubairskv <jubairskv@gmail.com>
2024-07-15 17:38:12 +0530

    Header cmponent done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,27 @@
-import './App.css';
+import "./App.css";
+import Header from "./components/Header";
+import Body from "./components/Body";
+
 
 function App() {
   return (
     <div className="App">
-      <h1 className='text-3xl'>jubair</h1>
+      <h1 className="">
+      <Header/>
+      <Body/>
+      </h1>
+      
+      {/**
+       *
+       *Head
+       *Body
+       *sidebar
+       *MenuItem
+       *MainContainer
+       *ButtonList
+       *VideoConatiner
+       *videoCard
+       */}
     </div>
   );
 }
