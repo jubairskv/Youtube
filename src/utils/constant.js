8f36e790df04b2e5142552b3cae2906a0784873a
jubairskv <jubairskv@gmail.com>
2024-07-16 15:23:13 +0530

    .env added

--- src/utils/constant.js
+++ src/utils/constant.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = process.env.REACT_APP_GOOGLE_API_KEY
+const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY
 
 export const YOUTUBE_VIDEOS_API =
   "https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=IN&key=" +
