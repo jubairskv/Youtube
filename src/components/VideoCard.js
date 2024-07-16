4d27245571dd01b28c66588a8043ddaec320af76
jubairskv <jubairskv@gmail.com>
2024-07-16 23:37:32 +0530

    Higher order component

--- src/components/VideoCard.js
+++ src/components/VideoCard.js
@@ -22,8 +22,7 @@ const VideoCard = ({ info }) => {
 export const AddVideoCard = ({ info }) => {
   return (
     <div className="p-1 m-1, border border-red-600">
-                                                      {/*Higher order component*/}
-      <VideoCard info={info} />{" "}
+        <VideoCard info={info} />              {/*Higher order component*/}
     </div>
   );
 };
