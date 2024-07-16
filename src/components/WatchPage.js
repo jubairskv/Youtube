d261939dd7e044b6bcb9690efa0cdaf7291e0450
jubairskv <jubairskv@gmail.com>
2024-07-16 16:50:51 +0530

    Watch page

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -1,9 +1,34 @@
-import React from 'react'
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { closeMenu } from "../utils/appSlice";
+import { useSearchParams } from "react-router-dom";
 
 const WatchPage = () => {
-  return (
-    <div>WatchPage</div>
-  )
-}
+  // const params = useParams();    //it wont wrok here so that we r going for search params in react-router-dom
+  // console.log(params)
 
-export default WatchPage
+  const [searchParams, setSearchParams] = useSearchParams();
+  console.log(searchParams.get("v"));
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(closeMenu());
+  }, []);
+  return (
+    <div className="px-5">
+      <iframe
+        width="1200"
+        height="600"
+        src={"https://www.youtube.com/embed/"+searchParams.get("v")}
+        title="அப்படி Farkhunda Malikzada-விற்கு அன்று நடந்தது என்ன ? | Saravanan Decodes"
+        frameBorder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerPolicy="strict-origin-when-cross-origin"
+        allowFullScreen
+      ></iframe>
+    </div>
+  );
+};
+
+export default WatchPage;
--- src/utils/appSlice.js
+++ src/utils/appSlice.js
@@ -1,17 +1,20 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const appslice = createSlice({
-    name:"app",
-    initialState:{
-        isMenuOpen:true
+  name: "app",
+  initialState: {
+    isMenuOpen: true,
+  },
+  reducers: {
+    toggleMenu: (state) => {
+      state.isMenuOpen = !state.isMenuOpen;
     },
-    reducers:{
-        toggleMenu:(state) =>{
-            state.isMenuOpen =!state.isMenuOpen;
-        }
-    }
-})
+    closeMenu: (state) => {
+      state.isMenuOpen = false;
+    },
+  },
+});
 
-export const {toggleMenu} = appslice.actions
+export const { toggleMenu , closeMenu } = appslice.actions;
 
 export default appslice.reducer;
