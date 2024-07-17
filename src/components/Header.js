3ed1870751c3f0a984cb48e57732cfd1a8095368
jubairskv <jubairskv@gmail.com>
2024-07-17 10:21:39 +0530

    Search Suggestion Done

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,19 +1,40 @@
 import React, { useState, useEffect } from "react";
 import { toggleMenu } from "../utils/appSlice";
-import { useDispatch } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { YOUTUBE_SEARCH_API } from "../utils/constant";
+import { CiSearch } from "react-icons/ci";
+import { cacheResults } from "../utils/searchSlice";
+
 
 const Header = () => {
   const [searchQuery, setSearchQuery] = useState("");
- 
+  const [Suggestion, setSuggestion] = useState([]);
+  const [showSuggestion, setShowSuggestion] = useState(false);
+
+  const dispatch = useDispatch();
+  const searchCache = useSelector((store) => store.search);
+
+  /**
+   * searchcache={
+   * "iphone:["iphone11","iphone14"]
+   * }
+   * searchQuery=iphone
+   */
 
   useEffect(() => {
     //API call
     //make an api call after every key press
     //but if the differrenece b/w 2 API calls is < 200ms
     //decline the API call
-    const timer = setTimeout(() => getSearchSuggestion(), 200);
-    return () => {   // clear the data when API call done in setTimeout
+    const timer = setTimeout(() => {
+      if (searchCache[searchQuery]) {
+        setSuggestion(searchCache[searchQuery]);
+      } else {
+        getSearchSuggestion();
+      }
+    }, 200);
+    return () => {
+      // clear the data when API call done in setTimeout
       clearTimeout(timer);
     };
   }, [searchQuery]);
@@ -34,26 +55,27 @@ const Header = () => {
    * -render the compoent
    * -make API call bcz of dependcies [state]
    * -start timer => make APi call after 200 ms
-   * 
-   * 
+   *
+   *
    * setTimeout(200) -make an api call
    */
 
   const getSearchSuggestion = async () => {
-   
     try {
-      console.log("API CALL - "+ searchQuery);
+      console.log("API CALL - " + searchQuery);
       const data = await fetch(YOUTUBE_SEARCH_API + searchQuery);
       const json = await data.json();
-      console.log(json[1]);
-    
+      setSuggestion(json[1]);
+      //console.log(json[1]);
+
+      //updaet the cache
+      dispatch(cacheResults({
+        [searchQuery]:json[1],
+      }))
     } catch (err) {
       console.log(err);
     }
   };
-
-  const dispatch = useDispatch();
-
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
   };
@@ -75,16 +97,32 @@ const Header = () => {
         </a>
       </div>
       <div className="col-span-10 px-10">
-        <input
-          className="w-1/2 border border-gray-400 p-2 rounded-l-full"
-          type="text"
-          placeholder="Search"
-          value={searchQuery}
-          onChange={(e) => setSearchQuery(e.target.value)}
-        />
-        <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
-          Search
-        </button>
+        <div>
+          <input
+            className="w-1/2 border border-gray-400 p-2 rounded-l-full"
+            type="text"
+            placeholder="Search"
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestion(true)}
+            onBlur={() => setShowSuggestion(false)}
+          />
+          <button className="border border-gray-400 px-5 py-2 rounded-r-full bg-gray-100">
+            Search
+          </button>
+          {showSuggestion && (
+            <div className="fixed bg-white py-2  w-[36rem] rounded-lg border border-gray-100">
+              <ul>
+                {Suggestion.map((s) => (
+                  <li className="px-5 py-2 flex hover:bg-gray-100 ">
+                    <CiSearch className="mt-[6px] mr-2" />
+                    {s}
+                  </li>
+                ))}
+              </ul>
+            </div>
+          )}
+        </div>
       </div>
       <div className="col-span-1">
         <img
---
+++ src/utils/searchSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const searchSlice = createSlice({
+  name: "search",
+  initialState: {},
+  reducers: {
+    cacheResults: (state, action) => {
+        state = Object.assign(state, action.payload);
+    },
+  },
+});
+
+export const { cacheResults } = searchSlice.actions;
+
+export default searchSlice.reducer;
+
+/**
+ * cache
+ * time complexity in array = o(n)
+ * time complexity in object = o(1)
+ * array.indexOf()
+ * [i,ip,iph,ipho,iphone]  -o(n)
+ * {
+ * i:
+ * ip:
+ * iph:
+ * ipho:
+ * iphon:
+ * iphone}
+ * new map()
+ */
