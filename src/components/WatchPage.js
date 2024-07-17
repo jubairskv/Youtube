d6cf6581890644a423dac42125b049d3439baa6d
jubairskv <jubairskv@gmail.com>
2024-07-17 18:38:52 +0530

    Comment section

--- src/components/WatchPage.js
+++ src/components/WatchPage.js
@@ -2,6 +2,7 @@ import React, { useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { closeMenu } from "../utils/appSlice";
 import { useSearchParams } from "react-router-dom";
+import CommentsContainer from "./CommentsContainer";
 
 const WatchPage = () => {
   // const params = useParams();    //it wont wrok here so that we r going for search params in react-router-dom
@@ -16,17 +17,20 @@ const WatchPage = () => {
     dispatch(closeMenu());
   }, []);
   return (
-    <div className="px-5">
-      <iframe
-        width="1200"
-        height="600"
-        src={"https://www.youtube.com/embed/"+searchParams.get("v")}
-        title="அப்படி Farkhunda Malikzada-விற்கு அன்று நடந்தது என்ன ? | Saravanan Decodes"
-        frameBorder="0"
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-        referrerPolicy="strict-origin-when-cross-origin"
-        allowFullScreen
-      ></iframe>
+    <div className="flex flex-col">
+      <div className="px-5">
+        <iframe
+          width="1200"
+          height="600"
+          src={"https://www.youtube.com/embed/" + searchParams.get("v")}
+          title="அப்படி Farkhunda Malikzada-விற்கு அன்று நடந்தது என்ன ? | Saravanan Decodes"
+          frameBorder="0"
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+      </div>
+      <CommentsContainer/>
     </div>
   );
 };
