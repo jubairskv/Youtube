65f7c98dc352b6a54e65e6d40e469497a42cc1ea
jubairskv <jubairskv@gmail.com>
2024-07-18 13:56:53 +0530

    live chat page

--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,9 +1,12 @@
-import React from 'react'
+import React from "react";
+import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
   return (
-    <div className='w-full h-[680px] ml-2 p-2 border border-b-gray-500'>LiveChat</div>
-  )
-}
+    <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg">
+      <ChatMessage />
+    </div>
+  );
+};
 
-export default LiveChat
+export default LiveChat;
