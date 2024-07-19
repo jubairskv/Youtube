0985fa85261e43a313a10e3df030b67d2fd17d83
jubairskv <jubairskv@gmail.com>
2024-07-19 09:32:37 +0530

    Live chat  page

--- src/components/ChatMessage.js
+++ src/components/ChatMessage.js
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
 
-const ChatMessage = () => {
+const ChatMessage = ({ name, message }) => {
   return (
-    <div>ChatMessage</div>
-  )
-}
+    <>
+      <div className="flex items-center shadow-sm p-1">
+        <img
+          className="h-8"
+          src="https://www.iconpacks.net/icons/2/free-user-icon-3296-thumb.png"
+          alt="user-icon"
+        />
+        <div className="px-2">
+          <span className="font-bold px-2">{name}</span>
+          <span>{message}</span>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default ChatMessage
+export default ChatMessage;
--- src/components/LiveChat.js
+++ src/components/LiveChat.js
@@ -1,10 +1,18 @@
-import React from "react";
+import React, { useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 
 const LiveChat = () => {
+  useEffect(() => {
+    //API or Long polling
+    const i = setInterval(() => {
+      //console.log("API polling");
+    }, 2000);
+
+    return () => clearInterval(i);
+  }, []);
   return (
     <div className="w-full h-[600px] ml-2 p-2 border border-black bg-slate-100 rounded-lg">
-      <ChatMessage />
+      <ChatMessage name="jubair kasim" message="hi this is jubiar🚀" />
     </div>
   );
 };
